49fc312c02487ea630824376b28de8a78b8a855c
Dzmitry Shalukhau <dima@equest.com>
2020-01-18 22:35:36 +0300

    Add test.

---
+++ src/test/java/ru/javaops/masterjava/matrix/MatrixUtilTest.java
@@ -0,0 +1,17 @@
+package ru.javaops.masterjava.matrix;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixUtilTest {
+
+    @org.junit.jupiter.api.Test
+    void singleThreadMultiply() {
+        int[][] matrixA = new int[][] {{2, -1}, {5, 3}};
+        int[][] matrixB = new int[][] {{3, 1, 0}, {2, -1, 5}};
+        int[][] expectedMatrixC = new int[][] {{4, 3, -5}, {21, 2, 15}};
+
+        int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+
+        assertArrayEquals(expectedMatrixC, matrixC);
+    }
+}
