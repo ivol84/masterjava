84e4c9b37e0568e7aec438bdeaba7e2155c039ae
Dzmitry Shalukhau <dima@equest.com>
2020-04-29 20:35:31 +0300

    Add MainXml.

---
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -0,0 +1,10 @@
+package ru.javaops.masterjava.xml;
+
+public class MainXml {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Enter project name");
+            return;
+        }
+    }
+}
